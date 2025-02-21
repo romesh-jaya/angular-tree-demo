6ce4b25a8167cb3e49ff4f4bf129ed102c1848ea
Romesh <romesh@medoment.com>
2025-02-21 11:30:13 +0530

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TreeModule } from 'primeng/tree';
 
+import { TableModule } from 'primeng/table';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, TreeModule],
+  imports: [BrowserModule, TreeModule, TableModule],
   providers: [],
   bootstrap: [AppComponent],
 })
